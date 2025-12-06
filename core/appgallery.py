088fd484e39abd9fbf5ccf357a87cfb35d9db25f
tianxiu2b2t <atianxiua@163.com>
2025-12-06 23:32:22 +0800

    重构应用画廊逻辑，优化类别处理和分享功能；移除冗余代码，增加全局变量以存储布局状态

--- core/appgallery.py
+++ core/appgallery.py
@@ -23,15 +23,18 @@ class AppInCategory:
 main_layout_res = None
 app_categories_res = None
 app_categories_size = None
-app_categories_branches: list[utils.JSON_PATH] = [
-    ['children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 2, 'children', 0, 'children', 0, 'children', 0, 'children', 1, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children'],
-    ['children', 0, 'children'],
-]
-apps_in_category_path: utils.JSON_PATH = ['children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 1, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children',]
-apps_in_category_app_name = ['children', 0, 'attributes', 'text']
+# app_categories_branches: list[utils.JSON_PATH] = [
+#     ['children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 2, 'children', 0, 'children', 0, 'children', 0, 'children', 1, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children'],
+#     ['children', 0, 'children'],
+# ]
+# apps_in_category_path: utils.JSON_PATH = ['children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 1, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children',]
+# apps_in_category_app_name = ['children', 0, 'attributes', 'text']
 
 skip_categories = []
 submit_username = ''
+share_layout_res = None
+share_with_gallery_view_page_res = None
+app_detail_layout_res = None
 
 async def is_main_page():
     main_screen = await hdc.get_main_screen_size()
@@ -101,8 +104,8 @@ async def wait_for_app_categories_page():
 
 async def click_app_categories():
     await wait_for_app_page()
-    # if await is_app_categories_page():
-    #     return
+    if await is_app_categories_page():
+        return
     paths = utils.find_json_value_as_path(app_categories_res, "分类")
     value = utils.find_json_value_by_path(app_categories_res, paths[0][:-1])
     bounds = utils.parse_bounds(value['bounds'])
@@ -114,125 +117,11 @@ async def get_app_categories():
     if app_categories_res is None:
         await wait_for_app_categories_page()
 
-    # with open("output.txt", "w", encoding="utf-8") as f:
-    #     daohang_paths = utils.find_json_value_as_path(app_categories_res, "出行导航")
-    #     children_paths = utils.find_json_value_as_path(app_categories_res, "儿童")
-    #     home_paths = utils.find_json_value_as_path(app_categories_res, "房产与装修")
-    #     diff_branch = []
-    #     for idx, (dps, cps, hps) in enumerate(zip(daohang_paths, children_paths, home_paths)):
-    #         for i, (dp, cp, hp) in enumerate(zip(dps, cps, hps)):
-    #             if dp == cp == hp:
-    #                 continue
-    #             diff_branch.append(i)
-
-    #             f.write(f"开始不同分支！{i}:\n")
-    #             f.write(f"daohang: {dp}\n")
-    #             f.write(f"children: {cp}\n")
-    #             f.write(f"home: {hp}\n")
-    #             f.write(f"common: {dps[:i]}\n")
-                
-
-    #         # 所以总结一下
-    #         f.write("总结：\n")
-    #         last_i = 0
-    #         for i in diff_branch:
-    #             f.write(f"分支{i}:\n")
-    #             f.write(f"{dps[last_i:i]}\n")
-    #             last_i = i
-
-    #         f.write("最后一组:\n")
-    #         f.write(f"{dps[last_i:]}")
-
-    #         diff_branch.clear()
-
-    # stable_count = 0
-    # results = set()
-    # while (stable_count := stable_count + 1) <= 3:
-    #     res = await hdc.dump_layout_to_json()
-    #     # 最后一个必然是
-
-
-    # result = []  # 最终收集到的所有唯一分类文本
-    # stable_count = 0  # 用于计数连续相同的次数
-    # required_stable_times = 2  # 要求连续相同的次数，达到则退出循环
-
-    # while stable_count < required_stable_times:
-    #     # 1. 每次循环都获取最新布局并解析
-    #     res = await hdc.dump_layout_to_json()
-    #     values = utils.find_json_value_by_path(res, app_categories_start_path)
-
-    #     # 2. 初始化本次循环的临时收集列表
-    #     current_loop_items = []
-    #     for value in values:
-    #         val = utils.find_json_value_by_path(value, app_categories_middle_path)
-    #         for v in val:
-    #             print
-    #             current_loop_items.append(utils.find_json_value_by_path(v, app_categories_end_path))
-
-    #     # 3. 判断本次收集的结果是否与“最终结果”相同
-    #     # 将列表排序后比较，避免顺序不同导致的误判
-    #     if sorted(current_loop_items) == sorted(result):
-    #         # 如果相同，稳定计数器加1
-    #         stable_count += 1
-    #         print(f"第 {stable_count} 次获取到相同结果。")
-    #     else:
-    #         # 如果不同，更新最终结果，并重置稳定计数器
-    #         result = current_loop_items[:]  # 使用切片创建副本
-    #         stable_count = 0
-    #         print(f"结果更新，当前分类数: {len(result)}")
-
-    #     main_screen_size = await hdc.get_main_screen_size()
-    #     await hdc.roll_pos_y(main_screen_size[0] * 0.5, main_screen_size[1] * 0.5, 60)
-
-    #     # 可选：增加短暂延迟，避免过于频繁的请求
-    #     await anyio.sleep(0.5)
-
-    # # 循环结束，输出最终稳定的结果
-    # print(f"页面稳定，最终收集到的分类: {result}")
-
 async def pull_app_in_categories():
     await wait_for_app_categories_page()
     global app_categories_res
     if app_categories_res is None:
         await wait_for_app_categories_page()
-
-
-    # with open("output.txt", "w", encoding="utf-8") as f:
-    #     daohang_paths = utils.find_json_value_as_path(app_categories_res, "出行导航")
-    #     children_paths = utils.find_json_value_as_path(app_categories_res, "儿童")
-    #     home_paths = utils.find_json_value_as_path(app_categories_res, "房产与装修")
-    #     with open("layouts.json", "w", encoding="utf-8") as f:
-    #         f.write(utils.json_dumps({
-    #             "daohang": daohang_paths,
-    #             "kids": children_paths,
-    #             "home": home_paths
-    #         }))
-    #     diff_branch = []
-    #     for idx, (dps, cps, hps) in enumerate(zip(daohang_paths, children_paths, home_paths)):
-    #         for i, (dp, cp, hp) in enumerate(zip(dps, cps, hps)):
-    #             if dp == cp == hp:
-    #                 continue
-    #             diff_branch.append(i)
-
-    #             f.write(f"开始不同分支！{i}:\n")
-    #             f.write(f"daohang: {dp}\n")
-    #             f.write(f"children: {cp}\n")
-    #             f.write(f"home: {hp}\n")
-    #             f.write(f"common: {dps[:i]}\n")
-                
-
-    #         # 所以总结一下
-    #         f.write("总结：\n")
-    #         last_i = 0
-    #         for i in diff_branch:
-    #             f.write(f"分支{i}:\n")
-    #             f.write(f"{dps[last_i:i]}\n")
-    #             last_i = i
-
-    #         f.write("最后一组:\n")
-    #         f.write(f"{dps[last_i:]}")
-
-    #         diff_branch.clear()
 
     main_screen_size = await hdc.get_main_screen_size()
     
@@ -241,17 +130,11 @@ async def pull_app_in_categories():
     while (stable_count := stable_count + 1) <= 3:
         current_clicked_categories = set()
         res = await hdc.dump_layout_to_json()
-        list_value = utils.find_json_value_by_path(res, utils.find_json_value_as_path(res, "List")[0])
-        buttons = utils.list_json_value_by_paths(list_value, utils.find_json_value_as_path(list_value, "Button"))
-        # listItems
-        # values = get_value_from_categories_res(res)
+        list_items = utils.find_json_value_by_prev_path(res, utils.find_json_value_as_path(res, "List")[-1], 2)
+        buttons = utils.list_json_value_by_prev_paths(list_items, utils.find_json_value_as_path(list_items, "Text"))
         if not buttons:
             continue
         for btn in buttons:
-            # try:
-            #     item = utils.find_json_value_by_path(btn, ['children', 0, 'children', 0, 'children', 0, 'attributes'])
-            # except Exception:
-            #     continue
             text = btn['text']
             if text is None or text == '' or text in clicked_categories:
                 continue
@@ -297,70 +180,52 @@ async def get_not_exists_apps(
 async def click_app_in_category_and_share(
     app_name: str,
     bounds: tuple[int, int, int, int]
-) -> bool:
+) -> Optional[str]:
     await hdc.click_by_bounds(bounds)
 
-    layout = await hdc.dump_layout_to_json()
-    back_btn = utils.find_json_value_by_prev_path(layout, utils.find_json_value_as_path(layout, "__NavdestinationField__BackButton__Back__")[0])['bounds']
-    share_btn = utils.find_json_value_by_prev_path(layout, utils.find_json_value_as_path(layout, "detail_share_menu")[0])['bounds']
+    global app_detail_layout_res, share_layout_res, share_with_gallery_view_page_res
+    if app_detail_layout_res is None:
+        app_detail_layout_res = await hdc.dump_layout_to_json()
+    app_detail_layout = app_detail_layout_res
+
+    back_btn = utils.find_json_value_by_prev_path(app_detail_layout, utils.find_json_value_as_path(app_detail_layout, "__NavdestinationField__BackButton__Back__")[0])['bounds']
+    share_btn = utils.find_json_value_by_prev_path(app_detail_layout, utils.find_json_value_as_path(app_detail_layout, "detail_share_menu")[0])['bounds']
     if back_btn is None or share_btn is None:
         logger.error(f"[{app_name}] 没有找到返回按钮或分享按钮")
-        return False
+        return
 
-    await anyio.sleep(0.75) # wait for network pull
+    # await anyio.sleep(0.75) # wait for network pull
 
 
-    # =============== Temp ===============
     await hdc.click_by_bounds(utils.parse_bounds(share_btn))
-    await anyio.sleep(0.5)
 
-    share_layout = await hdc.dump_layout_to_json()
-    # with open("share_layout.json", "w", encoding="utf-8") as f:
-    #     f.write(utils.json_dumps(share_layout))
+    if share_layout_res is None:
+        share_layout_res = await hdc.dump_layout_to_json()
+    share_layout = share_layout_res
 
     share_to_gallery_view = utils.find_json_value_by_prev_path(share_layout, utils.find_json_value_as_path(share_layout, "应用看板")[0])['bounds']
     await hdc.click_by_bounds(utils.parse_bounds(share_to_gallery_view))
 
-    share_with_gallery_view_page = await hdc.dump_layout_to_json()
+    if share_with_gallery_view_page_res is None:
+        share_with_gallery_view_page_res = await hdc.dump_layout_to_json()
+    share_with_gallery_view_page = share_with_gallery_view_page_res
+
     gallery_view_btn = utils.find_json_value_by_prev_path(share_with_gallery_view_page, utils.find_json_value_as_path(share_with_gallery_view_page, "按已有信息投稿到看板")[0])['bounds']
     await hdc.click_by_bounds(utils.parse_bounds(gallery_view_btn))
     
-    # hilog = await hdc.hilog("-e", '"share"', "-T", "JSAPP", "-z", "10000")
-    # _, pkgs = utils.parse_input_split_links_and_pkgs_tuple(hilog)
-    # pkg = pkgs[-1] # get last pkg
-    # logger.success(f"应用 [{app_name}] 包名 [{pkg}]")
-    # copy_btn = utils.find_json_value_by_prev_path(share_layout, utils.find_json_value_as_path(share_layout, "复制")[0])['bounds']
-    # # quit_share_btn = utils.find_json_value_by_prev_path(share_layout, utils.find_json_value_as_path(share_layout, "arrow_button")[0])['bounds']
-    # await hdc.click_by_bounds(utils.parse_bounds(copy_btn))
-    # clipboards += 1
-    # if clipboards % 10 == 0:
-    #     logger.warning(f"剪贴板已使用 {clipboards} 次")
-    # if clipboards >= clipboards_limit:
-    #     # wait for console user input
-    #     try:
-    #         input("请手动清空剪贴板，然后按回车继续")
-    #     except Exception:
-    #         pass
-    #     clipboards = 0 
+    hilog = await hdc.hilog("-e", '"share"', "-T", "JSAPP", "-z", "10000")
+    _, pkgs = utils.parse_input_split_links_and_pkgs_tuple(hilog)
+    if not pkgs:
+        logger.error(f"没有找到包名 [{app_name}]")
+        return
     
-    await anyio.sleep(1.2)
-    # await hdc.click_by_bounds(utils.parse_bounds(back_btn))
-
-
-
-    # =============== Temp ===============
-
-    # SHAREEEEEEEEEEEEEE
-    # and KNOCK BACK PREV PAGE
-    # 你会很好奇为什么我不写这个分享逻辑
-    # 因为某人在 2025/12/5 23:09:30 在某地铁上回家
-    # 然后我现在需要等他回到家，写完这个玩意出来*
-    # 我要哈气了
-    print(back_btn)
+    pkg = pkgs[-1] # get last pkg
+    logger.success(f"应用 [{app_name}] 包名 [{pkg}]")
+    
 
     await hdc.click_by_bounds(utils.parse_bounds(back_btn))
 
-    return True
+    return pkg
 
 async def pull_apps_in_categories():
     full_apps_list: set[str] = set()
@@ -369,13 +234,9 @@ async def pull_apps_in_categories():
         await anyio.sleep(0.25 + random.randint(3, 5) * 0.1)
         res = await hdc.dump_layout_to_json()
         list_items = utils.find_json_value_by_prev_path(res, utils.find_json_value_as_path(res, "List")[0], 2)
-        # text_zone
-        # items = utils.list_json_value_by_paths(list_items, utils.find_json_value_as_path(list_items, "text_zone"))
-        # data = utils.find_json_value_by_path(res, ['children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 1, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children', 0, 'children',])
+        
         current_apps_list: list[str] = []
         current_apps_bounds: dict[str, tuple[int, int, int, int]] = {}
-        with open("layout_apps.json", "w", encoding="utf-8") as f:
-            f.write(utils.json_dumps(list_items))
         for path in utils.find_json_value_as_path(list_items, "app_name"):
             item = utils.find_json_value_by_prev_path(list_items, path, 1)
             try:
@@ -411,7 +272,7 @@ async def pull_apps_in_categories():
 
         # roll down
         main_screen_size = await hdc.get_main_screen_size()
-        await hdc.roll_to_y(main_screen_size[0] * 0.5, main_screen_size[1] * 0.2, main_screen_size[1] * 0.7375)
+        await hdc.roll_to_y(main_screen_size[0] * 0.5, main_screen_size[1] * 0.2, main_screen_size[1] * 0.8)
 
         if not current_apps_list:
             stable_count += 1
@@ -430,21 +291,6 @@ async def next_pull_apps_in_categories():
 
     exit_btn = utils.parse_bounds(utils.find_json_value_by_path(apps_category_res, path)['bounds'])
     await hdc.click_by_bounds(exit_btn, 0.25)
-
-def get_value_from_categories_res(res: dict, idx: int = 0):
-    paths = app_categories_branches[idx]
-    value = utils.find_json_value_by_path(res, paths)
-    if value is None:
-        return []
-    result = []
-    next_idx = idx + 1
-    for v in value:
-        if not isinstance(v, dict) or next_idx >= len(app_categories_branches):
-            result.append(v)
-            continue
-        result.extend(get_value_from_categories_res(v, next_idx))
-    return result
-
         
 
 async def main(args):
